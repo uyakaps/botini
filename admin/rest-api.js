5ddaefc8e4b6d9f978be0e770633b18a634426de
RIMURUBOTZ <82255308+naylachan@users.noreply.github.com>
2022-10-24 15:51:14 +0800

    Add files via upload

---
+++ admin/rest-api.js
@@ -0,0 +1,11 @@
+/* Saya berterimakasih Kepada Penyedia Api */
+
+const api = require("caliph-api"); // https://www.npmjs.com/package/caliph-api
+const xa = require('xfarr-api') // https://www.npmjs.com/package/xfarr-api
+
+// (1) pecundang.herokuapp.com => Benny
+// (2) https://anabotofc.herokuapp.com => Undefined
+
+module.exports = { api, xa }
+
+// [❗] JANGAN UBAH APA APA DISINI!
